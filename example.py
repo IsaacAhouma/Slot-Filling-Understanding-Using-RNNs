2fefda674eaca9fcbf56bf8a8f2bcd5e533b736a
Tanoa Isaac Ahouma <e4n0b@ugrad.cs.ubc.ca>
2018-09-02 14:32:24 -0400

    updated main.py

--- example.py
+++ example.py
@@ -2,11 +2,14 @@
 from model_utilities import load_model
 
 
-def examples(my_model):
+#extended examples
+def examples_extended(my_model):
     print(my_model.evaluate("current weather in djakarta"))
     print(my_model.evaluate("I will be here until 6"))
     print(my_model.evaluate("I want to travel from miami to lagos"))
     print(my_model.evaluate("I want to leave accra for lagos on tuesday"))
+    print(my_model.evaluate("I want to go to accra from lagos on tuesday"))
+    print(my_model.evaluate("I want to travel to accra from lagos on tuesday"))
     print(my_model.evaluate("London weather"))
     print(my_model.evaluate("London expected weather"))
     print(my_model.evaluate("what time will the rain start in London"))
@@ -25,40 +28,23 @@ def examples(my_model):
     print(my_model.evaluate("Book a flight to Winnipeg from Gotham"))
     print(my_model.evaluate("Give me flights from New York City to Winnipeg"))
 
-
-def examples2(my_model):
+#examples from the project documentation
+def examples(my_model):
     print(my_model.evaluate("What's the weather like in London?"))
     print(my_model.evaluate("London weather"))
     print(my_model.evaluate("what’s the weather in Paris"))
     print(my_model.evaluate("temperature in San Francisco"))
     print(my_model.evaluate("I wanna fly from New York to Toronto"))
-    print(my_model.evaluate("I wanna fly to Toronto from New York"))
-    print(my_model.evaluate("Book a flight from Winnipeg to Halifax"))
-    print(my_model.evaluate("Book a flight to Halifax from Winnipeg"))
 
 
-# These are the three models that were saved on disk
-simple_gru = load_model('simple_gru')
-gru = load_model('gru')
-lstm = load_model('lstm')
-lstm_nopooling300 = load_model('lstm_nopooling300')
-lstm_nopooling = load_model('lstm_nopooling')
-gru_nopooling = load_model('gru_nopooling')
+
+# There are many trained models that were saved on disk, but this is the model with the best performance
 gru_nopooling_moredata2 = load_model('gru_nopooling_moredata2')  # best so far
 
 #look at prediction of different models on example queries
-examples(simple_gru)
-examples(gru)
-examples(lstm)
-examples(lstm_nopooling300)
-examples(lstm_nopooling)
-examples(gru_nopooling)
-
-examples2(gru_nopooling)
-
 best_model = gru_nopooling_moredata2
 examples(best_model)
-examples2(best_model)
+examples_extended(best_model)
 
 #look at summary of best_model
 best_model.summary
